b4617b3b28dd1d15267f525f01152a2589044474
Michael Dressner <mdressne@terpmail.umd.edu>
2021-02-20 11:25:13 -0500

    Added button listeners and deleted the preview

--- app/src/main/java/com/example/moonote/MyEntryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/moonote/MyEntryRecyclerViewAdapter.java
@@ -2,15 +2,18 @@ package com.example.moonote;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.moonote.Journal.Entry;
 import com.example.moonote.dummy.DummyContent.DummyItem;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -40,12 +43,29 @@ public class MyEntryRecyclerViewAdapter extends RecyclerView.Adapter<MyEntryRecy
         Calendar calendar = Calendar.getInstance();
         calendar.clear();
         calendar.setTimeInMillis(timestamp);
-        String time = DateFormat.getTimeInstance().format(calendar.getTime());
+        Date date = calendar.getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm a");
+        String time = simpleDateFormat.format(date);
 
         holder.mItem = mValues.get(position);
         holder.txtDateTime.setText(time);
-        holder.txtEntryPreview.setText(mValues.get(position).getBody());
+//        holder.txtEntryPreview.setText(mValues.get(position).getBody());
         holder.txtSentiment.setText(mValues.get(position).getSentiment().toString());
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent intent = new Intent(this, ViewEntryActivity.class);
+                // putExtra(the id of the entry);
+                // startActivity(intent);
+            }
+        });
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Pop up saying are you sure?????
+                // Do the deletion if user selects yes
+            }
+        });
     }
 
     @Override
@@ -56,16 +76,20 @@ public class MyEntryRecyclerViewAdapter extends RecyclerView.Adapter<MyEntryRecy
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView txtDateTime;
-        public final TextView txtEntryPreview;
+//        public final TextView txtEntryPreview;
         public final TextView txtSentiment;
+        public final ImageButton btnEdit;
+        public final ImageButton btnDelete;
         public Entry mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
             txtDateTime = (TextView) view.findViewById(R.id.txtDateTime);
-            txtEntryPreview = (TextView) view.findViewById(R.id.txtEntryPreview);
+//            txtEntryPreview = (TextView) view.findViewById(R.id.txtEntryPreview);
             txtSentiment = (TextView) view.findViewById(R.id.txtSentiment);
+            btnEdit = (ImageButton) view.findViewById(R.id.btnEdit);
+            btnDelete = (ImageButton) view.findViewById(R.id.btnDelete);
         }
 
         @Override
